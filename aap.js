011aabd3f68a40921ed7d355f25ddd862e0ae12b
Rajan Prajapati <rajanprajapati743@gmail.com>
2024-07-12 17:14:38 +0530

    update all

--- aap.js
+++ aap.js
@@ -1,47 +1,54 @@
-const express = require('express')
-const studentRoute = require('./api/routes/student')
-const facultyRoute = require('./api/routes/faculty')
-const user = require('./api/routes/user.js')
-const vechile = require('./api/routes/addVechile.js')
-const app = express()
+const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config()
-const bodyParser = require('body-parser')
-const cors = require('cors'); 
-const fileUpload = require('express-fileupload')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+require('dotenv').config();
 
-// connect with database  mongoDb
-mongoose.connect(process.env.MONGODB_URL)
-  .then(() => console.log('Connected!'))
-  .catch(error => {
-    console.error(error); 
-  });  
+const studentRoute = require('./api/routes/student');
+const facultyRoute = require('./api/routes/faculty');
+const userRoute = require('./api/routes/user');
+const vehicleRoute = require('./api/routes/addVechile');
 
-app.use(fileUpload({
-  useTempFiles:true
-}))
+const app = express();
+
+// Connect with MongoDB database
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB!'))
+.catch(error => {
+  console.error('MongoDB connection error:', error.message);
+});
+
+// Middleware
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
-
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST'], // Add the HTTP methods you want to allow
-  allowedHeaders: ['Content-Type', 'Authorization'], // Add headers you want to allow
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(fileUpload({
+  useTempFiles: true
 }));
 
+// Routes
+app.use('/student', studentRoute);
+app.use('/faculty', facultyRoute);
+app.use('/user', userRoute);
+app.use('/vehicle', vehicleRoute);
 
-app.use('/student',studentRoute)
-app.use('/faculty',facultyRoute)
-app.use('/user',user)
-app.use('/vechile',vechile)
+// 404 Error Handling
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Bad URL -> URL Not found'
+  });
+});
 
-app.use((req,res,next)=>{
-    res.status(404).json({
-        message:"Bad URL -> URL Not found"
-    })
-})
-
-
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: 'Internal Server Error'
+  });
+});
 
 module.exports = app;

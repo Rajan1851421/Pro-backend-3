ac1cbd7a3c4464876f837be010da5686cdad33a6
Rajan Prajapati <rajanprajapati743@gmail.com>
2024-09-30 01:13:39 +0530

    ok

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./aap')
 const server = http.createServer(app)
 const port = process.env.PORT || 3000
 
-
+server.use(cors());
 
 server.listen(port,()=>{
     console.log('App is Running on localhost:'+port)
